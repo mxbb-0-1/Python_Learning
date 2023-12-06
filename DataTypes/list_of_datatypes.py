8b4b7e069a89fcd9b6babaf0f725bc81a9ce4d6d
Mxbb_0_1 <anselmsherwin.w2020@vitstudent.ac.in>
2023-12-06 21:09:09 +0530

    First Commit

---
+++ DataTypes/list_of_datatypes.py
@@ -0,0 +1,62 @@
+# List of Data types in Python
+# 1. Numbers
+# 2. Strings
+# 3. List
+# 4. Tuple
+# 5. Dictionary
+# 6. Sets
+# 7. Boolean
+# 8. None
+# 9. Bytes and Bytearray
+
+# Numbers
+a = 10
+b = 10.5
+c = 10 + 5j
+print(a, b, c)
+print(type(a), type(b), type(c))
+
+# Strings
+name = "Python"
+print(name)
+print(type(name))
+
+# List
+lst = [10, 20, 30, 40, 50]
+print(lst)
+print(type(lst))
+
+# Tuple
+tup = (10, 20, 30, 40, 50)
+print(tup)
+print(type(tup))
+
+# Dictionary
+dct = {"name": "Python", "age": 30}
+print(dct)
+print(type(dct))
+
+# Sets
+st = {10, 20, 30, 40, 50}
+print(st)
+print(type(st))
+
+# Boolean
+b = True
+print(b)
+print(type(b))
+
+# None
+n = None
+print(n)
+print(type(n))
+
+# Bytes and Bytearray
+bt = b"Python"
+print(bt)
+print(type(bt))
+
+# Bytearray
+btarr = bytearray(bt)
+print(btarr)
+print(type(btarr))
